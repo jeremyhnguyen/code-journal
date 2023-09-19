00e7985112e444446483ef9b9044207c04d60e55
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-09-19 16:10:20 +0000

    completed user can create an entry #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+window.addEventListener('beforeunload', stringifyInput);
+
+function stringifyInput(string) {
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem('JS Storage', jsonData);
+}
+
+const getStorage = localStorage.getItem('JS Storage');
+if (getStorage !== null) {
+  data = JSON.parse(getStorage);
+}
